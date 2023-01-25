020bd97473d3529facf87915a397620ae34faad5
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-01-24 22:23:38 -0700

    made art cooler

--- components/home/art-banner.tsx
+++ components/home/art-banner.tsx
@@ -1,8 +1,8 @@
-import piece1 from '@/public/image-samples/prisms.jpg';
-import piece2 from '@/public/image-samples/chamber.jpg';
-import piece3 from '@/public/image-samples/lab.jpg';
-import piece4 from '@/public/image-samples/glyph.jpg';
-import piece5 from '@/public/image-samples/machine.jpg';
+import piece1 from '@/public/image-samples/structure.jpg';
+import piece2 from '@/public/image-samples/controls.jpg';
+import piece3 from '@/public/image-samples/saturn.jpg';
+import piece4 from '@/public/image-samples/network.jpg';
+import piece5 from '@/public/image-samples/lasers.jpg';
 import Artwork from './artwork';
 import styles from './art-banner.module.css';
 
